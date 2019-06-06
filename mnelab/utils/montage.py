467e211d7248fd090908440c64b175fcd2654f5e
TyWR <tanguy.viv@gmail.com>
2019-06-06 17:25:20 +0200

    Fix bug while reading xyz file

--- mnelab/utils/montage.py
+++ mnelab/utils/montage.py
@@ -1,9 +1,10 @@
+
 def xyz_to_montage(path, kind=''):
     """Reads and convert xyz positions to a mne montage type"""
     from mne.channels import Montage
     import numpy as np
 
-    n = int(open(path).readline().split(' ')[0])
+    n = int(open(path).readline().lstrip().split(' ')[0])
     coord = np.loadtxt(path, skiprows=1, usecols=(0, 1, 2), max_rows=n)
     names = np.loadtxt(path, skiprows=1, usecols=3, max_rows=n,
                        dtype=np.dtype(str))
