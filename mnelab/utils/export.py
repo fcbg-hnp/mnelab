6b84b98c68824df3e8cecbca5d6e33df9d03b0a1
Victor Férat <victor.ferat@live.Fr>
2019-06-27 16:01:54 +0200

    CHANGE export_sef

--- mnelab/utils/export.py
+++ mnelab/utils/export.py
@@ -1,19 +1,18 @@
 
 def export_sef(path, raw):
     """Export a raw mne file to a sef file."""
-
-    import numpy as np
     import struct
+    import numpy as np
+    import mne
 
     n_channels = len(raw.info['ch_names'])
     num_freq_frames = raw.n_times
     sfreq = raw.info['sfreq']
-
+    num_aux_electrodes = n_channels - len(mne.pick_types(info, meg=False, eeg=True, exclude=[""]))
     f = open(path, 'wb')
-    for car in 'SE01':
-        f.write(struct.pack('c', bytes(car, 'utf-8')))
+    f.write("SE01".encode('utf-8'))
     f.write(struct.pack('I', n_channels))
-    f.write(struct.pack('I', 0))
+    f.write(struct.pack('I', num_aux_electrodes))
     f.write(struct.pack('I', num_freq_frames))
     f.write(struct.pack('f', sfreq))
     f.write(struct.pack('H', 0))
@@ -24,14 +23,12 @@ def export_sef(path, raw):
     f.write(struct.pack('H', 0))
     f.write(struct.pack('H', 0))
 
-    for name in raw.info['ch_names']:
-        n = 0
-        for car in name:
-            f.write(struct.pack('c', bytes(car, 'utf-8')))
-            n += 1
-        while n < 8:
-            f.write(struct.pack('x'))
-            n += 1
+    ch_names = info["ch_names"]
+    for k in range(n_channels):
+        ch_name = ch_names[k]
+        ch_name = ch_name.ljust(8)
+        f.write(ch_name.encode('utf-8'))
+        print(ch_name)
 
     data = raw.get_data().astype(np.float32)
     data = np.reshape(data, n_channels * num_freq_frames, order='F')
